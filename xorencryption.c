afb2fd9b2e356d23c1c3d0629761e4d003b6b5f5
Sadie Simek <158243586+simeks18@users.noreply.github.com>
2025-11-28 14:36:02 -0500

    Add files via upload

---
+++ xorencryption.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "xorencryption.h"
+
+
+//define prime constants (every 4th prime from the first 25)
+int PRIMES[] = {2, 11, 23, 41, 59, 73, 97};
+int NUM_PRIMES = 7;
+
+//generate mask byte
+void xor_with_prime(unsigned char *data, size_t length, int prime) {
+        for (size_t i = 0, i < length; i++){
+        unsigned char mask = (unsigned char)((i * prime) & 0xFF);
+        data[i] = data[i] ^ mask; //XOR OPERATION with MASK
+        }
+}
